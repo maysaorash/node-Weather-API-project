e34a6700824f2521e1d8777afb45cf4535f37eef
Maysa Orazova <[maysaorash@gmail.com]>
2021-02-14 18:15:47 -0500

    node weather project

---
+++ index.js
@@ -0,0 +1,32 @@
+const request = require("request");
+const http = require("http");
+require("dotenv").config();
+
+const apiKey = process.env.apiKey;
+
+const myserver = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(
+    "<form method='post'> <input name='city'> <button>Submit</button> </form>"
+  );
+  let body = "";
+  req.on("data", function (chunk) {
+    //takes input value from DOM
+    body += chunk;
+    console.log(body);
+    const city = body
+    const url = "http://api.weatherstack.com/current?access_key=" + apiKey + "&query=" + city;
+    request(url, function (err, response, body) {
+      if (err) {
+        console.log("error:", error);
+      } else {
+        const info = JSON.parse(body);
+        console.log("body:", info.location.name);
+        res.end(
+          `Today ${info.current.temperature} degrees in ${info.location.name} `
+        );
+      }
+    });
+  });
+});
+myserver.listen(3000)
